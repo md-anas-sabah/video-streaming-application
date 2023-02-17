df0873776c2bd33f752472d516f5de7a22670587
Md Anas Sabah <mdanassabah@gmail.com>
2023-02-17 18:11:24 +0530

    added button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({name}) => {
+  return (
+    <div className="">
+      <button className="bg-gray-200 px-6 p-2 m-3 rounded-lg">{name}</button>
+    </div>
+  );
+};
+
+export default Button;
--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,5 +1,32 @@
+import Button from "./Button";
+
+const buttonList = [
+  "All",
+  "Live",
+  "Gaming",
+  "Anime",
+  "Songs",
+  "Soccer",
+  "Cricket",
+  "Cooking",
+  "Computer Programing",
+  "Manga",
+  "CSS",
+  "JavaScript",
+];
+
 const ButtonList = () => {
-  return <div className="">ButtonList</div>;
+  return (
+    <div className="flex ">
+      {buttonList.map((button) => {
+        return (
+          <div className="">
+            <Button name={button} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ButtonList;
