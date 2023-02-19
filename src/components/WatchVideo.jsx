ac347881914c708762a60db6d2101eb309c3c1e5
Md Anas Sabah <mdanassabah@gmail.com>
2023-02-19 18:01:29 +0530

    uodated component

--- src/components/WatchVideo.jsx
+++ src/components/WatchVideo.jsx
@@ -1,19 +1,31 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../redux/appSlice";
 
 const WatchVideo = () => {
   const dispatch = useDispatch();
 
-  const  id  = useParams();
-  console.log(id);
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
 
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
 
-  return <div className=""></div>;
+  return (
+    <div className="px-10 py-6">
+      <iframe
+        width="1000"
+        height="500"
+        src="https://www.youtube.com/embed/8pDqJVdNa44"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchVideo;
