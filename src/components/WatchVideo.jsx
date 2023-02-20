3d0cb11d68e7aa09c92c90ebd625db7112c2dcb4
Md Anas Sabah <mdanassabah@gmail.com>
2023-02-20 15:45:06 +0530

    enabled autoplay

--- src/components/WatchVideo.jsx
+++ src/components/WatchVideo.jsx
@@ -18,7 +18,12 @@ const WatchVideo = () => {
       <iframe
         width="1000"
         height="500"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        // src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        src={
+          "https://www.youtube.com/embed/" +
+          searchParams.get("v") +
+          "?autoplay=1"
+        }
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
