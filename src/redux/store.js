a67efc69a3d3e082dd2529f8dbed845e0d541532
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-01 19:22:29 +0530

    added chatSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
+import ChatSlice from "./ChatSlice";
 import searchSlice from "./searchSlice";
 
 const store = configureStore({
   reducer: {
     app: appSlice,
     search: searchSlice,
+    chat: ChatSlice,
   },
 });
 
